b31191fc5aa6f43a73ba2f4ab8337bbdd5c50b4d
Aurora Arendsen <288135956+aarendsen09-hue@users.noreply.github.com>
2026-07-11 20:44:53 -0400

    General app layout complete minus delete option for book

---
+++ java.js
@@ -0,0 +1,52 @@
+const myLibrary = [];
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(e) {
+    const title = prompt("What is the title?");
+    const author = prompt("Who is the author?");
+    const pages = prompt("How many pages?");
+    const read = prompt("Have you read it?");
+    addBookToLibrary(title, author, pages, read);
+    displayBooks();
+});
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    container.textContent = "";
+    for (var i = 0; i < myLibrary.length; i++) {
+        var bookView = document.createElement("div");
+        bookView.classList.add("card");
+        var title = document.createElement("p");
+        title.textContent = myLibrary[i].title;
+        bookView.appendChild(title);
+        var author = document.createElement("p");
+        author.textContent = myLibrary[i].author;
+        bookView.appendChild(author);
+        var pages = document.createElement("p");
+        pages.textContent = myLibrary[i].pages;
+        bookView.appendChild(pages);
+        var read = document.createElement("p");
+        read.textContent = myLibrary[i].read;
+        bookView.appendChild(read);
+        container.appendChild(bookView);
+    }
+}
+
+addBookToLibrary("Hamlet", "Shakespeare", 305, "not read yet");
+addBookToLibrary("Lord of the Flies", "Golding", 278, "read");
+addBookToLibrary("Northanger Abbey", "Austen", 342, "read");
+displayBooks();
